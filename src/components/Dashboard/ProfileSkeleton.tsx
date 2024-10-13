e3eeb93cfcff1d5d9dc5156770f23d822f753404
abhishekpandit <abhishekpandit1212@gmail.com>
2024-10-13 13:41:44 +0530

    fix/endpoint

--- src/components/Dashboard/ProfileSkeleton.tsx
+++ src/components/Dashboard/ProfileSkeleton.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Skeleton from '@mui/material/Skeleton';
+import { FaSlack } from 'react-icons/fa6';
 
 export default function ProfileSkeleton() {
     return (
