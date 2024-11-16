174d0e7025ae2db9f058f065c4e8a8cfd433c25e
abhishekpandit <abhishekpandit1212@gmail.com>
2024-11-16 17:14:54 +0530

    fix/data-fetch issue - feeds

--- src/app/api/thoughtcard/getcardsdata/route.ts
+++ src/app/api/thoughtcard/getcardsdata/route.ts
@@ -4,6 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 connect();
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
     try {
         const { searchParams } = new URL(request.url);
