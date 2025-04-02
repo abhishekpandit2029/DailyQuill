d1b6b13df9f1717d70c60fa88e5195cd7ae0b4fe
abhishekpandit <abhishekpandit1212@gmail.com>
2025-04-02 21:56:39 +0530

    integrate/full screen for persona section

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -1,16 +1,3 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useLayoutEffect } from "react";
-import { useAuth } from "@/context/AuthProvider";
-
 export default function AuthPage() {
-    const { isLoggedIn } = useAuth();
-    const { replace } = useRouter();
-    useLayoutEffect(() => {
-        if (isLoggedIn) {
-            replace("/login");
-        }
-    }, [replace, isLoggedIn]);
     return null;
 }
