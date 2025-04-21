89cf79744cec8db7ceee28de256b9db75655e634
abhishekpandit <abhishekpandit1212@gmail.com>
2025-04-21 22:27:23 +0530

    fix/button-ui-issue

--- src/components/Main/Layout/Navbar.tsx
+++ src/components/Main/Layout/Navbar.tsx
@@ -88,7 +88,7 @@ function Navbar() {
           :
           <Link href="/auth/login">
             <button
-              className={buttonClassName}
+              className="flex-none rounded-md bg-indigo-500 px-3 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             >
               Log in
             </button>
